6a8a0bdab868889343582f565ba3a39fe6ff14dc
AndyChoi4495 <yun4495@gmail.com>
2021-09-09 14:11:53 +0900

    remove

--- routes/book/view-router.js
+++ routes/book/view-router.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const moment = require('moment')
-const { error, chgStatus } = require('../../modules/util')
+const { error, chgStatus, relPath, isImg } = require('../../modules/util')
 const { pool } = require('../../modules/mysql-init')
 
 router.get('/:idx', async (req, res, next) => {
+	let sql, values
 	try {
-		const sql = "SELECT * FROM books WHERE idx=?"
-		const values = [req.params.idx]
+		sql = `
+		SELECT 
+		B.*, 
+		F.oriname, F.savename, F.idx AS id, 
+		F2.oriname AS oriname2, F2.savename AS savename2, F2.idx AS id2 
+		FROM books B 
+		LEFT JOIN files F 
+		ON B.idx = F.fidx AND F.fieldname = 'C'
+		LEFT JOIN files F2 
+		ON B.idx = F2.fidx AND F2.fieldname = 'U'
+		WHERE B.idx=?`
+		values = [req.params.idx]
 		const [[book]] = await pool.execute(sql, values)
 
 		book.createdAt = moment(book.createdAt).format('YYYY-MM-DD HH:mm:ss')
 		book.writer = book.writer || '미상'
 		book.status = chgStatus(book.status)
+		book.cover = book.savename ? relPath(book.savename) : null
+		book.upfile = book.savename2 ? relPath(book.savename2) : null
+		book.isImg = isImg(book.savename2 || '')
 
 		const title = '도서 상세 정보'
 		const description = '선택하신 도서의 상세 정보 입니다.'
