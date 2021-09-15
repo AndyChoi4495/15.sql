8cee22091e02da244cb87c818e1969a8a1b88ce4
AndyChoi4495 <yun4495@gmail.com>
2021-09-15 10:31:33 +0900

    ...

--- public/js/book/form.js
+++ public/js/book/form.js
@@ -12,17 +12,22 @@ function onSubmit(e) {
 	this.submit();
 }
 
-document.querySelector('#btRemoveCover').addEventListener('click', onRemoveFile)
-document.querySelector('#btRemoveFile').addEventListener('click', onRemoveFile)
+// 	/언어/book/		www.myshop.co.kr		:3000
+// 	/book/ 				api.myshop.co.kr		:3001
+// 	/							admin.myshop.co.kr	:3002
+
+document.querySelector('#btRemoveCover').addEventListener('click', onRemoveFile);
+document.querySelector('#btRemoveFile').addEventListener('click', onRemoveFile);
 function onRemoveFile(e) {
 	var idx = this.dataset['idx'];
 	var lang = this.dataset['lang'];
 	var parent = this.parentNode;
 	axios.delete('/'+lang+'/book/api/file', { params: { idx: idx } }).then(onSucess).catch(onError);
 	function onSucess(r) {
-	
+		if(r.data.code == 200) parent.remove();
 	}
 	function onError(err) {
-		
+		console.log(err);
+		console.log(err.response);
 	}
 }
